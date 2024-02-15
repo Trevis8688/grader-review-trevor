ca1576e716ed0b32b2cc3d5f8ce4b4c9f3d399e3
lotusrn <lotus.rs11@gmail.com>
2024-02-15 14:32:13 -0800

    alters

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -6,8 +6,41 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
-echo 'Finished cloning'
+echo 'Finished cloning!'
 
+if [ -f "student-submission/ListExamples.java" ];
+then
+      echo "File found!"
+else 
+      echo "File ListExamples.java is not found!"
+      exit 1
+fi
+
+# jars
+cp -r lib grading-area/
+# ListExamples
+cp student-submission/ListExamples.java grading-area/
+# TestListExamples
+cp TestListExamples.java grading-area/
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+       echo "Program failed to compile, see compile error above"
+       exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastLine= $(junit-output.txt | tail -n 2 | head -n 1)
+test=$(awk -F'[, ]' '(print $3)')
+failures=$(awk -F'[, ]' '(print $7)'
+
+successes=$((tests - failures))
+
+echo "Your score is $succeses / $tests"
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
